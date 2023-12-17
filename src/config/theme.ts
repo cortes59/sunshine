8ba6a3a0285a80d39286d95c478c3c79028583fb
cortes59 <corte.josew@gmail.com>
2023-12-17 14:14:47 -0600

    feat: install MUI and react hook form

---
+++ src/config/theme.ts
@@ -0,0 +1,8 @@
+import type {} from '@mui/x-date-pickers/themeAugmentation'
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  components: {},
+})
+
+export default theme
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,20 @@ import store from './store'
 import './index.css'
 
 import App from './App'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { ThemeProvider } from '@mui/material'
+import theme from './config/theme'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container!)
 
 root.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <App />
+      </LocalizationProvider>
+    </ThemeProvider>
   </Provider>
 )
